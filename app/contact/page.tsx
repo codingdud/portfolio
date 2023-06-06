3ac96e4a8cdbbe28a308cb5a6c1eefb3809cd603
codingdud <animeshk384@gmail.com>
2023-06-06 15:02:40 +0530

    update

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,27 +1,27 @@
 "use client";
-import { Github, Mail, Twitter } from "lucide-react";
+import { Github, Mail, Youtube } from "lucide-react";
 import Link from "next/link";
 import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
 
 const socials = [
 	{
-		icon: <Twitter size={20} />,
-		href: "https://twitter.com/chronark_",
-		label: "Twitter",
-		handle: "@chronark_",
+		icon: <Youtube size={20} />,
+		href: "https://youtube.com/codingdud",
+		label: "Youtube",
+		handle: "@codingdud",
 	},
 	{
 		icon: <Mail size={20} />,
-		href: "mailto:dev@chronark.com",
+		href: "codingdud@gmail.com",
 		label: "Email",
-		handle: "dev@chronark.com",
+		handle: "cogingdud@gmail.com",
 	},
 	{
 		icon: <Github size={20} />,
-		href: "https://github.com/chronark",
+		href: "https://github.com/codingdud",
 		label: "Github",
-		handle: "chronark",
+		handle: "codingdud",
 	},
 ];
 
