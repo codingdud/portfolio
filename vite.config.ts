8fbe62fdf1019a2e7be2d91714d3d5ea4f1c59d8
codingdud <codingdud@gmail.com>
2025-07-04 19:21:52 +0530

    added support for mermaind diagram

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,18 @@ import tailwindcss from '@tailwindcss/vite'
 import mdx from '@mdx-js/rollup';
 import remarkFrontmatter from 'remark-frontmatter';
 import remarkMdxFrontmatter from 'remark-mdx-frontmatter';
+
 // https://vite.dev/config/
 export default defineConfig({
   base: 'https://codingdud.github.io/portfolio/',
-  plugins: [tailwindcss(),react(),mdx({
+  plugins: [
+    tailwindcss(),
+    react(),
+    mdx({
       remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
-    })],
+      providerImportSource: '@mdx-js/react',
+    })
+  ],
   // Add this 'build' object
   build: {
     // Increase the limit to, for example, 1000KB (1MB)
